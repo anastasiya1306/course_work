0ef945b216babbc1220a864118864f0ee1fd545b
anastasiya <burykh_2000@mail.ru>
2023-03-31 01:49:38 +0300

    add connector.py

--- connector.py
+++ connector.py
@@ -1,3 +1,7 @@
+import json
+import os
+
+
 class Connector:
     """
     Класс коннектор к файлу, обязательно файл должен быть в json формате
@@ -19,6 +23,10 @@ class Connector:
         self.__data_file = value
         self.__connect()
 
+    @staticmethod
+    def is_file_exist(path):
+        return os.path.isfile(path)
+
     def __connect(self):
         """
         Проверка на существование файла с данными и
@@ -26,13 +34,32 @@ class Connector:
         Также проверить на деградацию и возбудить исключение
         если файл потерял актуальность в структуре данных
         """
-        pass
+        # проверка на наличие файла
+        if not self.is_file_exist:
+            raise FileNotFoundError('Файл не найден')
+        try:
+            with open(self.__data_file, 'r', encoding='utf-8') as file_path:
+                data = json.load(file_path)
+        except Exception:
+            with open(self.data_file, 'w', encoding='utf-8') as file_path:
+                json.dump([], file_path)
+
+
 
     def insert(self, data):
         """
         Запись данных в файл с сохранением структуры и исходных данных
         """
-        pass
+        with open(self.__data_file, 'r', encoding='utf8') as file:
+            file_path = json.load(file)
+
+        if isinstance(data, dict):
+            file_path.append(data)
+        elif isinstance(data, list):
+            file_path.extend(data)
+
+        with open(self.__data_file, 'w', encoding='utf8') as file:
+            json.dump(file_path, file, ensure_ascii=False, indent=4)
 
     def select(self, query):
         """
@@ -42,7 +69,15 @@ class Connector:
         {'price': 1000}, должно отфильтровать данные по полю price
         и вернуть все строки, в которых цена 1000
         """
-        pass
+        # считываем файл
+        with open(self.__data_file, 'r', encoding='utf8') as file:
+            file_path = json.load(file)
+        data = []
+        for i in file_path:
+            for key, value in query.items():
+                if i.get(key) == value:
+                    data.append(i)
+        return data
 
     def delete(self, query):
         """
@@ -50,7 +85,21 @@ class Connector:
         как в методе select. Если в query передан пустой словарь, то
         функция удаления не сработает
         """
-        pass
+        if not query:
+            return
+
+        with open(self.__data_file, 'r', encoding='utf8') as file:
+            file_path = json.load(file)
+        result = []
+        for i in file_path:
+            for key, value in query.items():
+                if i.get(key) == value:
+                    pass
+                else:
+                    result.append(i)
+
+        with open(self.__data_file, 'w', encoding='utf8') as f:
+            json.dump(result, f, ensure_ascii=False, indent=4)
 
 
 if __name__ == '__main__':
