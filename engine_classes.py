177f2402cb965ba3bb0f94fa3b5fa335c7462c02
anastasiya <burykh_2000@mail.ru>
2023-04-03 02:35:41 +0300

    add classes Engine, HH, SuperJob

--- engine_classes.py
+++ engine_classes.py
@@ -1,12 +1,13 @@
 from abc import ABC, abstractmethod
 from connector import Connector
 import requests
-from pprint import pprint
+import os
+import json
 
 
 class Engine(ABC):
     @abstractmethod
-    def get_request(self):
+    def get_request(self, keyword):
         pass
 
     @staticmethod
@@ -20,18 +21,17 @@ class HH(Engine):
     @staticmethod
     def get_salary(salary):
         """Обрабатываем salary(зарплата): выводим 'от' и 'до', если оба поля присутствуют, если одно из полей
-        отсутствует, то выволим то, что известно. Или выводим 0, если поле отсутствует"""
+        отсутствует, то выводим то, что известно. Или выводим 0, если поле отсутствует"""
         if salary:
             if salary.get('to') and salary.get('from'):
-                return {'to': salary['to'],
-                        'from': salary['from']}
+                return {'from': salary['from'],
+                        'to': salary['to']}
             elif salary.get('to') and not salary.get('from'):
-                return {'to': salary['to']}
+                return {'from': 0, 'to': salary['to']}
             elif salary.get('from') and not salary.get('to'):
-                return {'from': salary['from']}
+                return {'from': salary['from'], 'to': 0}
         else:
-            return 0
-
+            return {'from': 0, 'to': 0}
 
     def get_request(self, keyword):
         jobs = []
@@ -48,10 +48,40 @@ class HH(Engine):
                     "salary": self.get_salary(i.get('salary')),
                 })
 
-            return jobs
-class SuperJob(Engine):
-    def get_request(self):
-        pass
+        with open('hh_vacancy.json', 'w', encoding='utf-8') as file:
+            json.dump(jobs, file, indent=2, ensure_ascii=False)
 
-hh = HH()
-pprint(hh.get_request('python'))
+
+class SuperJob(Engine):
+    @staticmethod
+    def get_salary(salary):
+        """Обрабатываем salary(зарплата): выводим 'от' и 'до', если оба поля присутствуют, если одно из полей
+        отсутствует, то выволим то, что известно. Или выводим 0, если поле отсутствует"""
+        if salary.get('payment_to') and salary.get('payment_from'):
+            return {'from': salary['payment_from'],
+                    'to': salary['payment_to']}
+        elif salary.get('payment_to') and not salary.get('payment_from'):
+            return {'from': 0, 'to': salary['payment_to']}
+        elif salary.get('payment_from') and not salary.get('payment_to'):
+            return {'from': salary['payment_from'], 'to': 0}
+        return {'from': 0, 'to': 0}
+
+
+    def get_request(self, keyword):
+        jobs = []
+        for i in range(5):
+            api_key = os.getenv('superjob_key')
+            response = requests.get('https://api.superjob.ru/2.0/vacancies/', headers={'X-Api-App-Id': api_key},
+                                    params={"keywords": keyword, "count": 100,
+                                            "page": i})
+            data = response.json()
+            for i in data['objects']:
+                jobs.append({
+                    "name": i['profession'],
+                    "company_name": i['firm_name'],
+                    "url": i['link'],
+                    "description": i['candidat'],
+                    "salary": self.get_salary(i),
+                })
+        with open('sj_vacancy.json', 'w', encoding='utf-8') as file:
+            json.dump(jobs, file, indent=2, ensure_ascii=False)
